422a5e243ca4d59d6cd2e0a0b29a386af8136f15
JersonSS <JersonSS_2000@outlook.com>
2024-11-24 13:52:53 -0500

    insert, update y delete de Autor, AppBiblioteca

--- AppBiblioteca/BL/AutorBL.cs
+++ AppBiblioteca/BL/AutorBL.cs
@@ -13,5 +13,81 @@ namespace BL
     public class AutorBL
     {
         public AutorBL() { }
+
+        public void insert(AutorBE autorBE) 
+        {
+            //conexion DB
+            using (SqlConnection sqlConnection = new SqlConnection(ConnectionDB.SQLServer()))
+            {
+                sqlConnection.Open();
+
+                //SQL insert
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.CommandText = "insert into Autores values(@Apellido, @Nombre, @FechaNacimiento, @Nacionaliadad)";
+
+                //Load parameters
+                sqlCommand.Parameters.AddWithValue("@Apellido", autorBE.Apellido);
+                sqlCommand.Parameters.AddWithValue("@Nombre", autorBE.Nombre);
+                sqlCommand.Parameters.AddWithValue("@FechaNacimiento", autorBE.FechaNacimineto);
+                sqlCommand.Parameters.AddWithValue("@Nacionalidad", autorBE.Nacionalidad);
+
+
+                //execute
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+
+            }
+        }
+
+        public void update(AutorBE autorBE)
+        {
+            //conexion DB
+            using (SqlConnection sqlConnection = new SqlConnection(ConnectionDB.SQLServer()))
+            {
+                sqlConnection.Open();
+
+                //SQL insert
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.CommandText = "update Autores set Apellido=@Apellido, Nombre=@Nombre, FechaNacimiento=@FechaNacimiento, Nacionalidad=@Nacionaliadad where ID=@ID";
+
+                //Load parameters
+                sqlCommand.Parameters.AddWithValue("@Apellido", autorBE.Apellido);
+                sqlCommand.Parameters.AddWithValue("@Nombre", autorBE.Nombre);
+                sqlCommand.Parameters.AddWithValue("@FechaNacimiento", autorBE.FechaNacimineto);
+                sqlCommand.Parameters.AddWithValue("@Nacionalidad", autorBE.Nacionalidad);
+                sqlCommand.Parameters.AddWithValue("@ID", autorBE.ID);
+
+                //execute
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+
+            }
+        }
+
+
+        public void delete(int autorID)
+        {
+            //conexion DB
+            using (SqlConnection sqlConnection = new SqlConnection(ConnectionDB.SQLServer()))
+            {
+                sqlConnection.Open();
+
+                //SQL insert
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.CommandText = "delete from Autores where ID=@ID";
+
+                //Load parameters
+                sqlCommand.Parameters.AddWithValue("@ID",autorID);
+
+
+
+                //execute
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+
+            }
+        }
+
+
     }
 }
